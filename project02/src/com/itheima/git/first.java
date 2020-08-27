3b45d5df4d170553262afb37eb283807070488b0
Qinjie <806222929@qq.com>
2020-08-27 15:24:59 +0800

    把本地工程添加到git

---
+++ project02/src/com/itheima/git/first.java
@@ -0,0 +1,11 @@
+package com.itheima.git;
+
+public class first {
+    public static void main(String[] args) {
+        System.out.println("Hello git project");
+        System.out.println("Hello git project");
+        System.out.println("Hello git project");
+        System.out.println("Hello git project");
+        System.out.println("Hello git project");
+    }
+}
