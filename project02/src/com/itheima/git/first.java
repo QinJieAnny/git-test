6a69517f7c9216ea3f100812d6b7b8719fe8a927
Qinjie <806222929@qq.com>
2020-08-27 16:10:55 +0800

    增加了list的模板调试

--- project02/src/com/itheima/git/first.java
+++ project02/src/com/itheima/git/first.java
@@ -1,5 +1,8 @@
 package com.itheima.git;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class first {
     public static void main(String[] args) {
         System.out.println("Hello git project");
@@ -7,6 +10,14 @@ public class first {
         System.out.println("Hello git project");
         System.out.println("Hello git project");
         System.out.println("Hello git project");
+        List<String> list = new ArrayList<>();
+        list.add("qinjie");
+        list.add("anny");
+        System.out.println(list);
+        System.out.println("---------");
+        for (String s : list) {
+            System.out.println(s);
+        }
         //hello test
     }
     public void test(){
